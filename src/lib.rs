f890d680e39cd9380871a01d770c1aa9aef86842
Gihyun <giordnnuz@gmail.com>
2023-07-08 15:23:50 +0800

    Image output now has the dimensions of input width and height

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,20 @@
+use image::imageops::FilterType::Lanczos3;
 use image::{DynamicImage, ImageOutputFormat};
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn crop(bytes: &[u8], width: i32, height: i32) -> Result<Vec<u8>, JsValue> {
-    let mut image = image::load_from_memory(&bytes)
+pub fn crop(bytes: &[u8], width: u32, height: u32) -> Result<Vec<u8>, JsValue> {
+    let mut image = image::load_from_memory(bytes)
         .map_err(|e| JsValue::from_str(&format!("Failed to load the image: {}", e)))?;
     let cropped_image = crop_image(&mut image, width, height)
         .map_err(|e| JsValue::from_str(&format!("Failed to crop the image: {}", e)))?;
 
     let mut cropped_buffer = Vec::new();
-    let format = ImageOutputFormat::Jpeg(50);
-    cropped_image
+    let format = ImageOutputFormat::Jpeg(100);
+    let resized_cropped_image = cropped_image.resize(width, height, Lanczos3);
+
+    resized_cropped_image
         .write_to(&mut Cursor::new(&mut cropped_buffer), format)
         .map_err(|e| JsValue::from_str(&format!("Failed to write the image: {}", e)))?;
 
@@ -20,8 +23,8 @@ pub fn crop(bytes: &[u8], width: i32, height: i32) -> Result<Vec<u8>, JsValue> {
 
 fn crop_image(
     image: &mut DynamicImage,
-    target_width: i32,
-    target_height: i32,
+    target_width: u32,
+    target_height: u32,
 ) -> Result<DynamicImage, Box<dyn std::error::Error>> {
     let image_width = image.width();
     let image_height = image.height();
@@ -30,13 +33,11 @@ fn crop_image(
     let image_aspect_ratio = image_width as f64 / image_height as f64;
 
     let (x, y, width, height) = if image_aspect_ratio > aspect_ratio {
-        // Image is wider, crop horizontally
         let new_width = (image_height as f64 * aspect_ratio) as u32;
         let x = (image_width - new_width) / 2;
 
         (x, 0, new_width, image_height)
     } else {
-        // Image is taller, crop vertically
         let new_height = (image_width as f64 / aspect_ratio) as u32;
         let y = (image_height - new_height) / 2;
 
