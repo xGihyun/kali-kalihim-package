c63fda8fcf867c42d04ffe458cdf237595d6938a
Gihyun <giordnnuz@gmail.com>
2023-07-31 15:00:33 +0800

    Added battle card function, working on battle card effects

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,63 @@
+use battle_cards::battle_card::player_turn;
 use image::imageops::FilterType::Lanczos3;
 use image::{DynamicImage, ImageOutputFormat};
 use std::io::Cursor;
 use wasm_bindgen::prelude::*;
+// use webp::Encoder;
+use gloo_utils::format::JsValueSerdeExt;
+
+mod battle_cards;
+
+// use battle_cards::battle_card::simulate_strike;
+use battle_cards::models::{BattleCard, PlayerDamages, PlayerTurns};
+
+#[wasm_bindgen]
+pub fn card_battle(
+    player_1_cards_str: String,
+    player_2_cards_str: String,
+) -> Result<JsValue, JsValue> {
+    let player_1_cards_json = format!(r#"{player_1_cards_str}"#);
+    let player_2_cards_json = format!(r#"{player_2_cards_str}"#);
+
+    let player_1_cards: Vec<BattleCard> =
+        serde_json::from_str(&player_1_cards_json).expect("Failed to parse Player 1 cards");
+    let player_2_cards: Vec<BattleCard> =
+        serde_json::from_str(&player_2_cards_json).expect("Failed to parse Player 2 cards");
+
+    // let mut player_1_damage = 0;
+    // let mut player_2_damage = 0;
+
+    let mut player_1_turns: Vec<PlayerTurns> = vec![PlayerTurns::default(); 6];
+    let mut player_2_turns: Vec<PlayerTurns> = vec![PlayerTurns::default(); 6];
+
+    // Iterate over each players' cards and determine which cards dealt damage and which cards were cancelled
+    player_turn(
+        &player_1_cards,
+        &player_2_cards,
+        &mut player_1_turns,
+        &mut player_2_turns,
+    );
+    player_turn(
+        &player_2_cards,
+        &player_1_cards,
+        &mut player_2_turns,
+        &mut player_1_turns,
+    );
+
+    // Iterate over the player turns and apply the effects of the cards and add/deduct stats fr
+
+    // Change variable names later
+    // It works!
+    // Remember to return only the total damage dealt by each player later
+    let test = PlayerDamages {
+        player_1: player_1_turns,
+        player_2: player_2_turns,
+    };
+
+    let test2 = JsValue::from_serde(&test).expect("Failed to serialize");
+
+    Ok(test2)
+}
 
 #[wasm_bindgen]
 pub fn crop(bytes: &[u8], width: u32, height: u32) -> Result<Vec<u8>, JsValue> {
@@ -14,12 +70,20 @@ pub fn crop(bytes: &[u8], width: u32, height: u32) -> Result<Vec<u8>, JsValue> {
     let format = ImageOutputFormat::Jpeg(100);
     let resized_cropped_image = cropped_image.resize(width, height, Lanczos3);
 
+    // let webp_image = image_to_webp(bytes, width, height);
+
     resized_cropped_image
         .write_to(&mut Cursor::new(&mut cropped_buffer), format)
         .map_err(|e| JsValue::from_str(&format!("Failed to write the image: {}", e)))?;
 
     Ok(cropped_buffer)
 }
+
+// fn image_to_webp(bytes: &[u8], width: u32, height: u32) -> Vec<u8> {
+//     let encoded = Encoder::from_rgba(bytes, width, height).encode(100.0).to_vec();
+
+//     encoded
+// }
 
 fn crop_image(
     image: &mut DynamicImage,
